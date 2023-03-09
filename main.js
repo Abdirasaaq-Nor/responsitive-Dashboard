e303cafc53519583886db70664349f7425ebccdd
abdirsaq <rasaaq883@gmail.com>
2023-03-09 16:35:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const toggle  = document.querySelector('#toggle')
+const nav_item = document.querySelector('.nav_item')
+const right_bottom_side = document.querySelector('.right_bottom_side')
+
+
+
+
+toggle.addEventListener('click', () => {
+        if(nav_item.style.display == "block"){
+                nav_item.style.display = 'none'
+                right.style.margin = "0px 0px 0px 60px"
+              
+               
+              
+        }else{
+                nav_item.style.display = 'block'
+                right.style.margin = "0px 0px 0px 220px"
+    
+                
+                
+        }
+        
+})
+
+
+
+
+const image_toggle = document.querySelector('#image_toggle')
+const admin_info  = document.querySelector('.admin_info')
+image_toggle.addEventListener('click', () => {
+        if(admin_info.style.display == 'none'){
+                admin_info.style.display = 'block'
+        }else{
+                admin_info.style.display = 'none'
+        }
+})
+
+
+// toggle.addEventListener('click', () => {
+//         if(nav_item.classList.contains('nav_item')){
+//                 nav_item.style.display = 'none'
+//         } else{
+//                 nav_item.style.display = "block"
+//         }
+// })
